1cb4a09b5d5bcac3ee9c697171da66cbf74222bf
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2025-07-19 14:04:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Raashish Portfolio',
+  description: 'Created with prompts...',
   generator: 'v0.dev',
 }
 
